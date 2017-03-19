e1a309f388fb7d124b1e4c776c3f539023d4241a
Eugene Kegeles <zhenyakeg@gmail.com>
2017-03-20 01:54:34 +0300

    5 Done

--- Graph_Theory_SemiFinal_Preparartion/05_tree_bfs.py
+++ Graph_Theory_SemiFinal_Preparartion/05_tree_bfs.py
@@ -2,7 +2,6 @@ class task:
 
     task= '''Остовное дерево поиска в глубину. Реализация на Python 3.'''
 
-
 def read_graph_as_lists():
     N, M = tuple(map(int,input().split()))
     graph = [[] for i in range(N)]
@@ -11,18 +10,25 @@ def read_graph_as_lists():
         graph[a].append(b)
         graph[b].append(a)
     return graph
-def dfs(graph, start, path, used = None):
+def bfs(graph, start, path, used = None):
     if used == None:
         used = set()
     used.add(start)
-    for vertex in graph[start]:
-        if vertex not in used:
-            path.append((start, vertex))
-            dfs(graph, vertex, path, used)
+    Q = [start]
+    while Q:
+        vertex = Q.pop()
+        for neighbour in graph[vertex]:
+            if neighbour not in used:
+                path.append((vertex, neighbour))
+                used.add(neighbour)
+                Q.append(neighbour)
+    return path
+
+
 def build_tree(graph):
     tree = []
     used = set()
-    dfs(graph, 0, tree, used)
+    bfs(graph, 0, tree, used)
     return tree
 for edge in build_tree(read_graph_as_lists()):
     print(*edge)
