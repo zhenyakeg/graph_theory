11af007d64042076f107aef92cbf7c724f7d8750
Eugene Kegeles <zhenyakeg@gmail.com>
2017-03-20 00:28:18 +0300

    1 Done

---
+++ Graph_Theory_SemiFinal_Preparartion/02_count_components_dfs.py
@@ -0,0 +1,32 @@
+class task:
+
+    task= '''Считывание матрицы смежности орграфа и вывод списков смежности. Начальный граф задан списком ребер,
+    с изначальным заданием их числа и числа вершин'''
+
+
+def read_graph_as_lists():
+    N, M = tuple(map(int,input().split()))
+    graph = [[] for i in range(N)]
+    for edge in range(M):
+        a, b = tuple(map(int, input().split()))
+        graph[a].append(b)
+        graph[b].append(a)
+    return graph
+def dfs(graph, start, used = None):
+    if used == None:
+        used = set()
+    used.add(start)
+    for vertex in graph[start]:
+        if vertex not in used:
+            dfs(graph, vertex, used)
+def count_components(graph):
+    used = set()
+    number_of_components = 0
+    for vertex in range(len(graph)):
+        if vertex not in used:
+            dfs(graph, vertex, used)
+            number_of_components += 1
+    return number_of_components
+
+print(count_components(read_graph_as_lists()))
+
