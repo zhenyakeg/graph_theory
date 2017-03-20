ccb27e936522c7c0606957f74dbbee0fb3cbfc49
Eugene Kegeles <zhenyakeg@gmail.com>
2017-03-20 13:37:07 +0300

    5 Done

--- Graph_Theory_SemiFinal_Preparartion/09_krascal_algorithm.py
+++ Graph_Theory_SemiFinal_Preparartion/09_krascal_algorithm.py
@@ -9,11 +9,20 @@ def krascal(graph_as_edges, num_of_vertexes):
     tree_weight = 0
     edges = sorted(graph_as_edges, key=lambda x: x[2])
     names_of_components = [i for i in range(num_of_vertexes)] # имена компонент связности.
+    current_component = names_of_components[edges[0][0]]
     for vertex1, vertex2, cost in edges:
+        previous_comp1 = names_of_components[vertex1]
+        previous_comp2 = names_of_components[vertex2]
         if names_of_components[vertex1] != names_of_components[vertex2]:
+            names_of_components[vertex1] = names_of_components[vertex2]
             tree.append((vertex1, vertex2))
             tree_weight += cost
-        for i in range(num_of_vertexes):
-            if names_of_components[i] == names_of_components[vertex2]:
-                names_of_components[i] = names_of_components[vertex1]
+            for i in range(num_of_vertexes):
+                if names_of_components[i] == previous_comp1 or names_of_components[i] == previous_comp2:
+                    names_of_components[i] = names_of_components[vertex1]
     return tree_weight, tree
+G =read_as_edges_list()
+res = krascal(G[0], G[1])
+print(res[0])
+for edge in res[1]:
+    print(*edge)
